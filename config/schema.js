63634eb6814a5e54c73c15f3c44ea90fd7bd7786
BadoniShreyansh <badonishreyansh@gmail.com>
2025-07-08 08:27:11 +0530

    new table to database

--- config/schema.js
+++ config/schema.js
@@ -1,8 +1,23 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, jsonb, pgTable, varchar, boolean } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
   subscriptionId: varchar(),
+});
+
+export const coursesTable = pgTable("courses", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  cid: varchar().notNull(),
+  name: varchar(),
+  description: varchar(),
+  noOfChapters: integer(),
+  includeVideo: boolean().default(false),
+  level: varchar().notNull(),
+  category: varchar(),
+  courseJson: jsonb(),
+  userEmail: varchar("userEmail")
+    .references(() => usersTable.email)
+    .notNull(),
 });
